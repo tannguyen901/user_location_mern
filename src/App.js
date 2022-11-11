23419213830a0639719e19bf614eecf33019fe12
tan nguyen <tannguyen901.tannguyen901@gmail.com>
2022-11-11 14:30:34 -0800

    routing initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import Places from "./places/pages/Places";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <Places />
+        </Route>
+        <Redirect path="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/places/pages/Places.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Places = () => {
+    return <h1>Places new</h1>
+};
+
+export default Places;
