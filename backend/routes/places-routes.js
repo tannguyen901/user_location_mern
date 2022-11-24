32be37dc1ecc0f02d2c3d9b8afe5f2b5237f808a
unknown <tannguyen901@gmail.com>
2022-11-23 17:07:13 -0800

    added more get requests for places routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,10 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('GET Request in Places');
-    res.json({message: "it works"});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empite State Building",
+    description: "One of hte most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;
