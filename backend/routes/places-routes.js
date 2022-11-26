08705b9f266fa1c6460f066375041d376e1bda5d
unknown <tannguyen901@gmail.com>
2022-11-26 09:48:42 -0800

    some routing, modelling, controllers

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,35 +1,13 @@
 const express = require("express");
 
+const placesControllers = require("../controllers/places-controller");
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empite State Building",
-    description: "One of hte most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-  res.json({ place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  res.json({ place });
-});
+router.post('/', placesControllers.createPlace)
 
 module.exports = router;
