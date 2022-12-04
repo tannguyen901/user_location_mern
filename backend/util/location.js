9f291f9d5717466445e9a0533abd27da8bd21a94
unknown <tannguyen901@gmail.com>
2022-12-03 20:06:37 -0800

    cleanup in location.js and adding source

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,3 +1,44 @@
+//"https://developers.arcgis.com/labs/rest/search-for-an-address/"
+const axios = require('axios');
+ 
+const HttpError = require('../models/http-error');
+ 
+const getCoordsForAddress = async (address) => {
+    const response = await axios.get(
+        'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates',
+        {
+            params: {
+                f: 'json',
+                singleLine: address,
+                outFields: 'Match_addr,Addr_type'
+            }
+        }
+        );
+        
+        const data = response.data;
+        
+        // Check if no matches were found
+        if (!data || data.candidates.length === 0) {
+            const error = new HttpError(
+                'Could not find location for the specified address.',
+                422
+    );
+    throw error;
+}
+
+// Get Latitude
+const lat = response.data.candidates[0].location.y;
+// Get Longitude
+const lng = response.data.candidates[0].location.x;
+
+return {
+    lat,
+    lng
+};
+};
+module.exports = getCoordsForAddress;
+
+
 // const axios = require("axios");
 // const API_KEY = "AIzaSyAXr6nyP7MdQzD0-HzTqx4NtTnn29Ue6L0";
 
@@ -26,43 +67,3 @@
 // }
 
 // module.exports = getCoordsForAddress;
-
-const axios = require('axios');
- 
-const HttpError = require('../models/http-error');
- 
-const getCoordsForAddress = async (address) => {
-  const response = await axios.get(
-    'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates',
-    {
-      params: {
-        f: 'json',
-        singleLine: address,
-        outFields: 'Match_addr,Addr_type'
-      }
-    }
-  );
- 
-  const data = response.data;
- 
-  // Check if no matches were found
-  if (!data || data.candidates.length === 0) {
-    const error = new HttpError(
-      'Could not find location for the specified address.',
-      422
-    );
-    throw error;
-  }
- 
-  // Get Latitude
-  const lat = response.data.candidates[0].location.y;
-  // Get Longitude
-  const lng = response.data.candidates[0].location.x;
- 
-  return {
-    lat,
-    lng
-  };
-};
- 
-module.exports = getCoordsForAddress;
